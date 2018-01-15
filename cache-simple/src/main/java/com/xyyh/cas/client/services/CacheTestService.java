d19cd652fedbb08cc1b6e5d8a607eb3856830381
xz <ldwqh0@outlook.com>
2018-01-15 21:56:49 +0800

    缓存示例代码

--- cache-simple/src/main/java/com/xyyh/cas/client/services/CacheTestService.java
+++ cache-simple/src/main/java/com/xyyh/cas/client/services/CacheTestService.java
@@ -2,4 +2,10 @@ package com.xyyh.cas.client.services;
 
 public interface CacheTestService {
 	public String get(String id);
+
+	public String delete(String id);
+
+	public String save(String id, String value);
+
+	public String update(String id, String value);
 }
--- cache-simple/src/main/java/com/xyyh/cas/client/services/support/SimpleCacheTestServiceImpl.java
+++ cache-simple/src/main/java/com/xyyh/cas/client/services/support/SimpleCacheTestServiceImpl.java
@@ -1,11 +1,15 @@
 package com.xyyh.cas.client.services.support;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +18,12 @@ import com.xyyh.cas.client.services.CacheTestService;
 @Service
 public class SimpleCacheTestServiceImpl implements CacheTestService {
 	private static final Logger logger = LoggerFactory.getLogger(SimpleCacheTestServiceImpl.class);
+
+	private final Map<String, String> enties = new HashMap<>();
+
+	public SimpleCacheTestServiceImpl() {
+		enties.put("1", "this no 1");
+	}
 
 	@Autowired
 	private CacheManager cacheManager;
@@ -27,7 +37,27 @@ public class SimpleCacheTestServiceImpl implements CacheTestService {
 		logger.info("The cacheManager is" + cacheManager);
 		// 当数据不是从cache里面获取时，打印日志
 		logger.info("Get value by id=" + id + ", The time is " + time);
-		return "Get value by id=" + id + ", The time is " + time;
+		return "Get value by id=" + id + ",the value is " + enties.get(id);
+	}
+
+	@Override
+	@CacheEvict(cacheNames = "test")
+	public String delete(String id) {
+		return enties.remove(id);
+	}
+
+	@Override
+	@CachePut(cacheNames = "test", key = "#id")
+	public String save(String id, String value) {
+		logger.info("save value " + value + " with key " + id);
+		enties.put(id, value);
+		return value;
+	}
+
+	@Override
+	@CachePut(cacheNames = "test", key = "#id")
+	public String update(String id, String value) {
+		return enties.put(id, value);
 	}
 
 }
